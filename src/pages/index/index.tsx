02023bf1103f52aaa899859b7d2cce4b36ac1292
Sergiu Ghenciu <1024gs@gmail.com>
2021-04-26 16:44:41 +0100

    Update user page (add user form)

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -10,7 +10,39 @@ const IndexPage = () => {
       <div className="container">
         <FormNav />
         <br />
-        User
+        <form>
+          <div className="form-group">
+            <label htmlFor="name">Name: *</label>
+            <input type="text" className="form-control" id="name" required />
+          </div>
+          <div className="form-group">
+            <label htmlFor="role">Role:</label>
+            <input type="text" className="form-control" id="role" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email: *</label>
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              pattern=".+@.+"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password: *</label>
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              minLength="10"
+              required
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
       </div>
     </div>
   );
