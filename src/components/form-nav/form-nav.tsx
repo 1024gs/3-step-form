e58f5335e47d6a4b31925b97e918463bd1902f0c
Sergiu Ghenciu <1024gs@gmail.com>
2021-04-26 17:06:57 +0100

    Update form-nav component (dynamic active class)

--- src/components/form-nav/form-nav.tsx
+++ src/components/form-nav/form-nav.tsx
@@ -1,21 +1,26 @@
 import React from '../../../vendors/react';
-import {Link} from '../../../vendors/wouter/index';
+import {Link, useLocation} from '../../../vendors/wouter/index';
 
 const FormNav = () => {
+  const [location] = useLocation();
+
+  const linkClassName = (path) =>
+    'nav-link' + (location === path ? ' active' : '');
+
   return (
     <ul className="nav nav-pills nav-fill">
       <li className="nav-item">
-        <Link className="nav-link active" href="/">
+        <Link className={linkClassName('/')} href="/">
           User
         </Link>
       </li>
       <li className="nav-item">
-        <Link className="nav-link disabled" href="/privacy">
+        <Link className={linkClassName('/privacy')} href="/privacy">
           Privacy
         </Link>
       </li>
       <li className="nav-item">
-        <Link className="nav-link disabled" href="/done">
+        <Link className={linkClassName('/done')} href="/done">
           Done
         </Link>
       </li>
