335e7b8f68f3d4d12134ee6869be9464425b9fd0
Sergiu Ghenciu <1024gs@gmail.com>
2021-04-26 19:50:37 +0100

    Update form-nav component (disable the next steps)

--- src/components/form-nav/form-nav.tsx
+++ src/components/form-nav/form-nav.tsx
@@ -1,26 +1,30 @@
 import React from '../../../vendors/react';
 import {Link, useLocation} from '../../../vendors/wouter/index';
+import useForm from '../../hooks/use-form/use-form';
 
 const FormNav = () => {
   const [location] = useLocation();
+  const [form] = useForm();
 
-  const linkClassName = (path) =>
-    'nav-link' + (location === path ? ' active' : '');
+  const linkClassName = (path, step) =>
+    'nav-link' +
+    (location === path ? ' active' : '') +
+    (form.step < step ? ' disabled' : '');
 
   return (
-    <ul className="nav nav-pills nav-fill">
+    <ul className="nav nav-pills nav-fill form-nav">
       <li className="nav-item">
-        <Link className={linkClassName('/')} href="/">
+        <Link className={linkClassName('/', 1)} href="/">
           User
         </Link>
       </li>
       <li className="nav-item">
-        <Link className={linkClassName('/privacy')} href="/privacy">
+        <Link className={linkClassName('/privacy', 2)} href="/privacy">
           Privacy
         </Link>
       </li>
       <li className="nav-item">
-        <Link className={linkClassName('/done')} href="/done">
+        <Link className={linkClassName('/done', 3)} href="/done">
           Done
         </Link>
       </li>
