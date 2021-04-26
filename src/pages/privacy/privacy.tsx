3dcab4fda93790c81bfb7d43be88f19d03311ee2
Sergiu Ghenciu <1024gs@gmail.com>
2021-04-26 16:58:22 +0100

    Update privacy page (add subscription form)

--- src/pages/privacy/privacy.tsx
+++ src/pages/privacy/privacy.tsx
@@ -10,7 +10,37 @@ const PrivacyPage = () => {
       <div className="container">
         <FormNav />
         <br />
-        Privacy
+
+        <form>
+          <div className="form-group">
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value="updates"
+                id="updates"
+              />
+              <label className="form-check-label" htmlFor="updates">
+                Receive updates about Tray.io product by email
+              </label>
+            </div>
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value="communication"
+                id="communication"
+              />
+              <label className="form-check-label" htmlFor="communication">
+                Receive communication by email for other products created by the
+                Tray.io team
+              </label>
+            </div>
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
       </div>
     </div>
   );
